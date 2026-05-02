d7fe0b855e36006e36f4f83f13849cd691cb352b
LanTru <trungocbalan@gmail.com>
2026-05-02 14:05:27 +0700

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Text;
 
 namespace BaitapCSLT
@@ -11,6 +11,10 @@ namespace BaitapCSLT
             Console.InputEncoding = Encoding.UTF8;
 
             BT1();
+            BT2();
+            BT3();
+            BT4();
+            BT5();
 
             Console.ReadKey();
         }
@@ -37,5 +41,76 @@ namespace BaitapCSLT
                 Console.WriteLine("Wrong Character");
         }
 
+        static void BT2()
+        {
+            Console.WriteLine("Bang gia tri cua ham so x = y^2 + 2y + 1");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("{0,10} | {1,10}", "y", "x");
+            Console.WriteLine("---------------------------------------");
+            for (int y = -5; y <= 5; y++)
+            {
+                int x = (y * y) + (2 * y) + 1;
+
+                Console.WriteLine("{0,10} | {1,10}", y, x);
+            }
+        }
+
+        static void BT3()
+        {
+            float distance;
+            float hour, min, sec;
+            float timeSec;
+            float mps, kph, mph;
+
+            Console.Write("Nhap quang duong (met): ");
+            distance = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Nhap thoi gian - Gio: ");
+            hour = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Nhap thoi gian - Phut: ");
+            min = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Nhap thoi gian - Giay: ");
+            sec = Convert.ToSingle(Console.ReadLine());
+            timeSec = (hour * 3600) + (min * 60) + sec;
+            mps = distance / timeSec;
+            kph = (distance / 1000.0f) / (timeSec / 3600.0f);
+            mph = kph / 1.609f;
+            Console.WriteLine("Ket qua:");
+            Console.WriteLine("Toc do (m/s) la: {0}", mps);
+            Console.WriteLine("Toc do (km/h) la: {0}", kph);
+            Console.WriteLine("Toc do (mph) la: {0}", mph);
+        }
+
+        static void BT4()
+        {
+            float r;
+            float pi = 3.1415926535f;
+            Console.Write("Radius: ");
+            r = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Surface area of the sphere: " + 4 * pi * (r * r));
+            Console.WriteLine("Volume of the sphere: " + 4f / 3f * pi * (r * r * r));
+        }
+
+        static void BT5()
+        {
+            char symbol;
+            Console.Write("Input a symbol: ");
+            symbol = Convert.ToChar(Console.ReadLine());
+            if ((symbol == 'a') || (symbol == 'e') || (symbol == 'i') ||
+                (symbol == 'o') || (symbol == 'u'))
+            {
+                Console.WriteLine("It's a lowercase vowel.");
+            }
+            else if ((symbol >= '0') && (symbol <= '9'))
+            {
+                Console.WriteLine("It's a digit.");
+            }
+            else
+            {
+                Console.Write("It's another symbol.");
+            }
+        }
     }
 }
